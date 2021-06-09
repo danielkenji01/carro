4844da4787000b4fbaad96efffd16ceabeb354f7
danielkenji01 <daniel_kenji_1@hotmail.com>
2021-06-09 19:49:42 -0300

    Initial commit

---
+++ src/test/java/com/fatec/carro/CarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
